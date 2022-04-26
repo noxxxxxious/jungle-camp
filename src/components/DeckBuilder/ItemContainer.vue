09d2b06e411394b9eec157af8a4e4a7a0d456c32
Sean Fried <sean.fried@gmail.com>
2022-04-25 22:52:08 -0500

    Added item image on drop to ItemContainer

--- src/components/DeckBuilder/ItemContainer.vue
+++ src/components/DeckBuilder/ItemContainer.vue
@@ -1,18 +1,41 @@
 <template>
-  <div class="item-container" @drop="onDrop" @dragover.prevent @dragenter.prevent>
+  <div class="item-container" :style="itemImageStyle" @drop="onDrop" @dragover.prevent @dragenter.prevent>
 
   </div>
 </template>
 
 <script>
 export default {
+  data: () => ({
+    item: null,
+    hasDropped: false
+  }),
+
   methods: {
     onDrop (event) {
       event.preventDefault()
       const data = event.dataTransfer.getData("text")
+
+      // remove "card-" from element ID
       const cardName = data.split('-')
       cardName.shift()
-      console.log(cardName.join(' '))
+
+      this.item = this.getItem(cardName.join(' '))
+    },
+
+    getItem (inItemName) {
+      return this.$store.getters.getItemList.filter((item) => inItemName === item.name)[0]
+    },
+
+    getImgUrl (inItem) {
+      return require('@/assets/images/' + inItem.name.replace(/\s/g, '_').replace(/'/g, "")  + '.png')
+    }
+  },
+
+  computed: {
+    itemImageStyle () {
+      if (!this.item) { return '' }
+      return `background-image: url('${this.getImgUrl(this.item)}');`
     }
   }
 }
@@ -28,5 +51,7 @@ export default {
     border-top-left-radius: 10px;
     border-bottom-right-radius: 10px;
     background: #333;
+    background-position: 50% 20%;
+    background-size: 130%;
   }
 </style>

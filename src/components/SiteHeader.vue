d141161f5d3c4401fc946273e45ef4bd2d4d9e1a
Sean Fried <sean.fried@gmail.com>
2022-06-16 23:27:50 -0500

    Styled Discord login button and user avatar

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -2,18 +2,21 @@
   <div class="header">
     <span class="site-title">The Jungle Camp</span>
     <nav>
-      <div v-if="userInfo">
+      <div v-if="userInfo" class="logged-in-menu">
         <ul>
           <li ref="itemDescBtn" class="button active" @click="navChange('itemDescriptions')">Item Descriptions</li>
           <li ref="deckBuilderBtn" class="button" @click="navChange('deckBuilder')">Deck Builder</li>
         </ul>
         <div class="user-banner">
-          <span>{{ userInfo.name }}#{{ userInfo.discriminator }}</span>
-          <img :src="`https://cdn.discordapp.com/avatars/${ userInfo.discordId }/${ userInfo.avatar }.jpg`" >
+          <span>{{ userInfo.name }}</span>
+          <img :src="`https://cdn.discordapp.com/avatars/${ userInfo.discordId }/${ userInfo.avatar }.jpg?size=64`" >
         </div>
       </div>
-      <div v-else>
-        <a href="https://discord.com/oauth2/authorize?client_id=985091844658561026&redirect_uri=http%3A%2F%2Flocalhost%3A3001%2Fapi%2Fauth%2Fdiscord%2Fredirect&response_type=code&scope=identify">Log in with Discord</a>
+      <div v-else class="not-logged-in-menu">
+        <a class="button button--discord" href="https://discord.com/oauth2/authorize?client_id=985091844658561026&redirect_uri=http%3A%2F%2Flocalhost%3A3001%2Fapi%2Fauth%2Fdiscord%2Fredirect&response_type=code&scope=identify">
+          <img class="discord-logo" src="@/assets/images/Discord_Logo.svg">
+          <span>Log in with Discord</span>
+        </a>
       </div>
     </nav>
   </div>
@@ -52,7 +55,7 @@ export default {
     background-color: #202020;
     box-shadow: 0px 5px 5px #0004;
     z-index: 20;
-    justify-content: space-between;
+    align-items: center;
   }
 
   .site-title {
@@ -60,7 +63,17 @@ export default {
     font-size: 1.5rem;
   }
 
+  nav {
+    flex-grow: 1;
+  }
+
+  .logged-in-menu {
+    display: flex;
+    justify-content: space-between;
+  }
+
   ul {
+    /* display: inline-block; */
     list-style: none;
     margin: 0;
   }
@@ -89,6 +102,45 @@ export default {
   .button.active:hover {
     cursor: initial;
     box-shadow: none;
+  }
+
+  .user-banner {
+    height: 31px;
+    display: flex;
+    align-items: center;
+  }
+
+  .user-banner img {
+    height: 50px;
+    border-radius: 50%;
+    border: 2px solid white;
+    margin-left: 10px;
+  }
+
+  .not-logged-in-menu {
+    display: flex;
+    justify-content: flex-end;
+  }
+
+  .button--discord {
+    display: flex;
+    gap: 5px;
+    border-color: #5865F2;
+    background-color: #5865F2;
+    color: white;
+    text-decoration: none;
+    align-content: center;
+  }
+
+  .button--discord:hover {
+    color: #fff;
+    background-color: #717cf3;
+    box-shadow: 0px 0px 5px 5px #5865F222;
+    cursor: pointer;
+  }
+
+  .discord-logo {
+    height: 20px;
   }
 
 </style>
